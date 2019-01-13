d3f3c9a2219ecf01dace8b7c715ce07255f45719
Dason Goh <dason@detrack.com>
2019-01-13 23:04:13 +0800

    Add memory management options

--- graphhopper.sh
+++ graphhopper.sh
@@ -206,7 +206,7 @@ else
 fi
 
 if [ "$JAVA_OPTS" = "" ]; then
-  JAVA_OPTS="-Xmx60000m -Xms60000m -server -Xconcurrentio -XX:+HeapDumpOnOutOfMemoryError"
+  JAVA_OPTS="-Xmx60000m -Xms60000m -server -Xconcurrentio -XX:+HeapDumpOnOutOfMemoryError -XX:+UseG1GC -XX:MetaspaceSize=100M"
 fi
 
 ensureOsm
